fe4c253187345d53eb0059309a54a390ee45f044
kunalkalbande <dineshr@synerzipune.local>
2018-02-12 11:47:45 +0530

    Added project MaasPrompt

---
+++ MaasPrompt/MaasPrompt/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MaasPrompt.Startup))]
+namespace MaasPrompt
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
