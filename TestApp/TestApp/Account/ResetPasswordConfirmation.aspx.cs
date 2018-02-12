0f4ca99e0ed5476d4d3ef157e31f27e75a2d0c96
Kunal Kalbande <kunalk@synerzipune.local>
2018-02-12 14:02:12 +0530

    new code

--- TestApp/TestApp/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace TestApp.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
---
+++ TestWebApp/TestWebApp/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace TestWebApp.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
